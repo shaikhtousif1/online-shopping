723f229edbe14d5d432d05a4998cc7e5f930d9a9
Tousif Shaikh <shaikhtousif1@gmail.com>
2019-09-11 19:13:07 +0530

    successfully loaded static Resources

---
+++ onlineshopping/src/main/java/net/tou/mantri/onlineshopping/controller/PageController.java
@@ -0,0 +1,20 @@
+package net.tou.mantri.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to SPr");
+		return mv;
+	}
+
+}
